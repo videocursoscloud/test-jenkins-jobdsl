6083ca2f22ac4afe874b3c864b92bc8c8a9d7e19
Miguel Arranz <mijackatmijackdotnet>
2020-10-18 21:43:35 +0100

    changes

---
+++ jobs/test-jenkins-docker.groovy
@@ -0,0 +1,13 @@
+job('Seed All') {
+  scm {
+    git ('https://github.com/videocursoscloud/test-jenkins-jobdsl.git')
+  }
+  steps {
+    dsl {
+      external('jobs/*.groovy')  
+      // default behavior
+      // removeAction('IGNORE')      
+      removeAction('DELETE')
+    }
+  }
+}
