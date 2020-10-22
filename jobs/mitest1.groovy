ca929f5ef365124118f170a365659f2924e25b6e
Miguel Arranz <miguel@miguel.miguel>
2020-10-22 11:37:36 -0700

    changes

---
+++ jobs/mitest1.groovy
@@ -0,0 +1,17 @@
+pipelineJob("mitest1") {
+  description()
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+            url("https://github.com/videocursoscloud/test-jenkins-docker.git")
+          }
+          branch("master")
+        }
+      }
+      scriptPath("Jenkinsfile")
+    }
+  }
+}
+
